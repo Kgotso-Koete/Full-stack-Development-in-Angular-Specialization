fb3c36ff2bdf5584c2512ce37279e01a4ec13ba0
“Kgotso-Koete” <kgkoete@gmail.com>
2019-12-11 17:18:08 +0200

    Angular week 2: Single Page Applications Part 1

--- 2. Front-End JavaScript Frameworks - Angular/Angular/conFusion/src/app/app.module.ts
+++ 2. Front-End JavaScript Frameworks - Angular/Angular/conFusion/src/app/app.module.ts
@@ -15,6 +15,8 @@ import { MatButtonModule } from "@angular/material/button";
 import { DishdetailComponent } from "./dishdetail/dishdetail.component";
 
 import { DishService } from "./services/dish.service";
+import { PromotionService } from "./services/promotion.service";
+
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
 import { AboutComponent } from "./about/about.component";
@@ -45,7 +47,7 @@ import { AppRoutingModule } from "./app-routing/app-routing.module";
     MatButtonModule,
     AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [DishService, PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
