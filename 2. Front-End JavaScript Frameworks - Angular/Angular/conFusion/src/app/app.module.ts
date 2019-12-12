8eda49ded97c3ccf1d336a163c55b6d2d064f591
“Kgotso-Koete” <kgkoete@gmail.com>
2019-12-12 22:55:11 +0200

    Angular week 3: Template Forms Part 1

--- 2. Front-End JavaScript Frameworks - Angular/Angular/conFusion/src/app/app.module.ts
+++ 2. Front-End JavaScript Frameworks - Angular/Angular/conFusion/src/app/app.module.ts
@@ -11,6 +11,10 @@ import { MatGridListModule } from "@angular/material/grid-list";
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
 import { MatDialogModule } from "@angular/material/dialog";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { FormsModule } from "@angular/forms";
 //services
 import { DishService } from "./services/dish.service";
 import { PromotionService } from "./services/promotion.service";
@@ -50,7 +54,11 @@ import { LoginComponent } from "./login/login.component";
     MatCardModule,
     MatButtonModule,
     AppRoutingModule,
-    MatDialogModule
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    FormsModule
   ],
   providers: [DishService, PromotionService, LeaderService],
   entryComponents: [LoginComponent],
