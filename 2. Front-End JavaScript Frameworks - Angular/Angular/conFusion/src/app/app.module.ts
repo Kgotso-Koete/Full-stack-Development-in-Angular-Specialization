6b99d0270e9301bf837b8d6ca942179eb222b1f8
“Kgotso-Koete” <kgkoete@gmail.com>
2019-12-13 11:39:31 +0200

    Angular week 3: Reactive Forms Part 1

--- 2. Front-End JavaScript Frameworks - Angular/Angular/conFusion/src/app/app.module.ts
+++ 2. Front-End JavaScript Frameworks - Angular/Angular/conFusion/src/app/app.module.ts
@@ -15,6 +15,10 @@ import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { MatCheckboxModule } from "@angular/material/checkbox";
 import { FormsModule } from "@angular/forms";
+import { MatSelectModule } from "@angular/material/select";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { ReactiveFormsModule } from "@angular/forms";
+
 //services
 import { DishService } from "./services/dish.service";
 import { PromotionService } from "./services/promotion.service";
@@ -58,7 +62,10 @@ import { LoginComponent } from "./login/login.component";
     MatFormFieldModule,
     MatInputModule,
     MatCheckboxModule,
-    FormsModule
+    FormsModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    ReactiveFormsModule
   ],
   providers: [DishService, PromotionService, LeaderService],
   entryComponents: [LoginComponent],
