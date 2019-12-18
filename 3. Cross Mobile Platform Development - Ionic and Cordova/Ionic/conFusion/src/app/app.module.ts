1129a9b8420e2af171066417c0feda8f4f3a3ca7
“Kgotso-Koete” <kgkoete@gmail.com>
2019-12-19 00:36:37 +0200

    Ionic & Cordova week 1: Ionic Angular

--- 3. Cross Mobile Platform Development - Ionic and Cordova/Ionic/conFusion/src/app/app.module.ts
+++ 3. Cross Mobile Platform Development - Ionic and Cordova/Ionic/conFusion/src/app/app.module.ts
@@ -1,25 +1,39 @@
+// Modules
 import { BrowserModule } from "@angular/platform-browser";
 import { ErrorHandler, NgModule } from "@angular/core";
 import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
-
+import { HttpModule } from "@angular/http";
+// Pages
 import { MyApp } from "./app.component";
 import { HomePage } from "../pages/home/home";
 import { AboutPage } from "../pages/about/about";
 import { MenuPage } from "../pages/menu/menu";
 import { ContactPage } from "../pages/contact/contact";
-
+// Native Components
 import { StatusBar } from "@ionic-native/status-bar";
 import { SplashScreen } from "@ionic-native/splash-screen";
+// Providers
+import { DishProvider } from "../providers/dish/dish";
+import { LeaderProvider } from "../providers/leader/leader";
+import { PromotionProvider } from "../providers/promotion/promotion";
+import { ProcessHttpmsgProvider } from "../providers/process-httpmsg/process-httpmsg";
+// Shared
+import { baseURL } from "../shared/baseurl";
 
 @NgModule({
   declarations: [MyApp, HomePage, AboutPage, MenuPage, ContactPage],
-  imports: [BrowserModule, IonicModule.forRoot(MyApp)],
+  imports: [BrowserModule, IonicModule.forRoot(MyApp), HttpModule],
   bootstrap: [IonicApp],
   entryComponents: [MyApp, HomePage, AboutPage, MenuPage, ContactPage],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: ErrorHandler, useClass: IonicErrorHandler }
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    DishProvider,
+    LeaderProvider,
+    PromotionProvider,
+    ProcessHttpmsgProvider,
+    { provide: "BaseURL", useValue: baseURL }
   ]
 })
 export class AppModule {}
